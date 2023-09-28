674881cbc19981dc8a5195251189fa0d26c4d893
Josué Boisvert <josue.boisvert@gmail.com>
2023-09-28 17:18:17 -0400

    small adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,13 @@
 #include "glm/vec2.hpp"
 #include <vector>
 
+
 static glm::vec2 push;
 static Rectangle liquid;
+
+const float pointRadius = 8.0;
+const int screenWidth = 840;
+const int screenHeight = 680;
 
 class Application {
 private:
@@ -19,7 +24,7 @@ public:
     Application() : running(true), particles({}), elapsed_time(0.0f) {
         SetConfigFlags(FLAG_MSAA_4X_HINT);
         SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-        InitWindow(840, 680, "Pikuma Physics");
+        InitWindow(screenWidth, screenHeight, "Pikuma Physics");
 
         SetTargetFPS(60.0f);
 
@@ -38,17 +43,18 @@ public:
 
     void Setup() {
         particles.emplace_back(50, 100, 1.0);
-        particles.emplace_back(25, 150, 0.5);
-        particles.emplace_back(100, 160, 3.0);
-        particles.emplace_back(45, 170, 4.0);
-        particles.emplace_back(75, 180, 5.0);
+//        particles.emplace_back(25, 150, 0.5);
+//        particles.emplace_back(100, 160, 3.0);
+//        particles.emplace_back(45, 170, 4.0);
+//        particles.emplace_back(75, 180, 5.0);
 
         push = vec2(0.0f);
 
-        liquid.x = 0;
-        liquid.y = 340;
-        liquid.width = 840;
-        liquid.height = 340;
+//        // Liquid rectangle
+//        liquid.x = 0;
+//        liquid.y = screenHeight / 2;
+//        liquid.width = screenWidth;
+//        liquid.height = screenHeight / 2;
     }
 
     void Input() {
@@ -75,7 +81,7 @@ public:
             particle.AddForce(push);
 
             // Applying friction
-            glm::vec2 frictionForce = Force::GenerateFrictionForce(particle.Velocity, 10.0 * PIXEL_PER_METER);
+            glm::vec2 frictionForce = Force::GenerateFrictionForce(particle.Velocity, 10.0f * PIXEL_PER_METER);
             particle.AddForce(frictionForce);
 
 //            // Applying forces depending on the liquid
@@ -100,20 +106,22 @@ public:
 
         // Boundary collisions
         for (auto &particle: particles) {
-            if (particle.Position.x > 836.0f) {
-                particle.Position.x = 836.0f;
+            float widthBoundary = screenWidth - pointRadius;
+            float heightBoundary = screenHeight - pointRadius;
+            if (particle.Position.x > widthBoundary) {
+                particle.Position.x = widthBoundary;
                 particle.Velocity.x *= -0.9f;
             }
-            if (particle.Position.x < 4.0f) {
-                particle.Position.x = 4.0f;
+            if (particle.Position.x < pointRadius) {
+                particle.Position.x = pointRadius;
                 particle.Velocity.x *= -0.9f;
             }
-            if (particle.Position.y > 676.0f) {
-                particle.Position.y = 676.0f;
+            if (particle.Position.y > heightBoundary) {
+                particle.Position.y = heightBoundary;
                 particle.Velocity.y *= -0.9f;
             }
-            if (particle.Position.y < 4.0f) {
-                particle.Position.y = 4.0f;
+            if (particle.Position.y < pointRadius) {
+                particle.Position.y = pointRadius;
                 particle.Velocity.y *= -0.9f;
             }
         }
@@ -126,7 +134,7 @@ public:
         DrawRectangleRec(liquid, BLUE);
 
         for (auto &particle: particles) {
-            DrawCircle((int)particle.Position.x, (int)particle.Position.y, 4.0f, WHITE);
+            DrawCircle((int)particle.Position.x, (int)particle.Position.y, pointRadius, WHITE);
         }
 
         EndDrawing();
