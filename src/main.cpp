023564940c6c275b1725fda4d52538fb02d6f039
Josué Boisvert <josue.boisvert@gmail.com>
2023-10-03 16:11:56 -0400

    debug draw circle shapes

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "physic_constants.h"
 #include "forces.h"
 #include "glm/vec2.hpp"
+#include "glm/gtx/rotate_vector.hpp"
 #include <vector>
-
 
 static glm::vec2 push;
 static Rectangle liquid;
@@ -47,27 +47,21 @@ public:
 
         push = glm::vec2(0.0f);
 
-//        // Liquid rectangle
-//        liquid.x = 0;
-//        liquid.y = screenHeight / 2;
-//        liquid.width = screenWidth;
-//        liquid.height = screenHeight / 2;
-
         springRestDistance = 80.0f;
         springStiffness = 500.0f;
     }
 
     void Input() {
         push = glm::vec2(0.0f);
-        if(IsKeyDown(KEY_S)) push.y += 15.0f * PIXEL_PER_METER;
-        if(IsKeyDown(KEY_W)) push.y -= 15.0f * PIXEL_PER_METER;
-        if(IsKeyDown(KEY_D)) push.x += 15.0f * PIXEL_PER_METER;
-        if(IsKeyDown(KEY_A)) push.x -= 15.0f * PIXEL_PER_METER;
+        if (IsKeyDown(KEY_S)) push.y += 15.0f * PIXEL_PER_METER;
+        if (IsKeyDown(KEY_W)) push.y -= 15.0f * PIXEL_PER_METER;
+        if (IsKeyDown(KEY_D)) push.x += 15.0f * PIXEL_PER_METER;
+        if (IsKeyDown(KEY_A)) push.x -= 15.0f * PIXEL_PER_METER;
 
-        if(IsKeyDown(KEY_UP)) springRestDistance += 5.0f;
-        if(IsKeyDown(KEY_DOWN)) springRestDistance -= 5.0f;
-        if(IsKeyDown(KEY_RIGHT)) springStiffness += 2.0f;
-        if(IsKeyDown(KEY_LEFT)) springStiffness -= 2.0f;
+        if (IsKeyDown(KEY_UP)) springRestDistance += 5.0f;
+        if (IsKeyDown(KEY_DOWN)) springRestDistance -= 5.0f;
+        if (IsKeyDown(KEY_RIGHT)) springStiffness += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) springStiffness -= 2.0f;
 
         PollInputEvents();
     }
@@ -91,22 +85,6 @@ public:
             body.AddForce(weightForce);
         }
 
-
-        // SPRING
-
-        glm::vec2 sp = Force::GenerateSpringForce(
-                bodies[0],
-                glm::vec2(screenWidth * 0.5f, 50.0f),
-                springRestDistance,
-                springStiffness
-        );
-        bodies[0].AddForce(sp);
-        for (int i = 1; i < bodies.size(); ++i) {
-            sp = Force::GenerateSpringForce(bodies[i], bodies[i - 1], springRestDistance, springStiffness);
-            bodies[i].AddForce(sp);
-            bodies[i -1].AddForce(-sp);
-        }
-
         // Force integration
         for (auto &body: bodies) {
             body.Integrate(GetFrameTime());
@@ -114,24 +92,26 @@ public:
 
         // Boundary collisions
         for (auto &body: bodies) {
-            float radius = dynamic_cast<CircleShape*>(body.shape)->radius;
-            float widthBoundary = screenWidth - radius;
-            float heightBoundary = screenHeight - radius;
-            if (body.Position.x > widthBoundary) {
-                body.Position.x = widthBoundary;
-                body.Velocity.x *= -0.9f;
-            }
-            if (body.Position.x < radius) {
-                body.Position.x = radius;
-                body.Velocity.x *= -0.9f;
-            }
-            if (body.Position.y > heightBoundary) {
-                body.Position.y = heightBoundary;
-                body.Velocity.y *= -0.9f;
-            }
-            if (body.Position.y < radius) {
-                body.Position.y = radius;
-                body.Velocity.y *= -0.9f;
+            if (body.shape->GetType() == CIRCLE) {
+                float radius = dynamic_cast<CircleShape *>(body.shape)->radius;
+                float widthBoundary = screenWidth - radius;
+                float heightBoundary = screenHeight - radius;
+                if (body.Position.x > widthBoundary) {
+                    body.Position.x = widthBoundary;
+                    body.Velocity.x *= -0.9f;
+                }
+                if (body.Position.x < radius) {
+                    body.Position.x = radius;
+                    body.Velocity.x *= -0.9f;
+                }
+                if (body.Position.y > heightBoundary) {
+                    body.Position.y = heightBoundary;
+                    body.Velocity.y *= -0.9f;
+                }
+                if (body.Position.y < radius) {
+                    body.Position.y = radius;
+                    body.Velocity.y *= -0.9f;
+                }
             }
         }
     }
@@ -140,19 +120,34 @@ public:
         ClearBackground(DARKGREEN);
         BeginDrawing();
 
-//        DrawRectangleRec(liquid, BLUE);
+        static float angle = 0.0f;
 
         // Draw Circle shape
         for (auto &body: bodies) {
-            DrawCircle((int)body.Position.x,
-                       (int)body.Position.y,
-                       dynamic_cast<CircleShape*>(body.shape)->radius,
-                       WHITE);
+            if (body.shape->GetType() == CIRCLE) {
+                float radius = dynamic_cast<CircleShape *>(body.shape)->radius;
+
+                DrawCircleLines((int) body.Position.x,
+                                (int) body.Position.y,
+                                radius,
+                                WHITE);
+                glm::vec2 line = glm::vec2(radius, 0.0f);
+                line = glm::rotate(line, angle);
+                line += body.Position;
+                DrawLine((int) body.Position.x,
+                         (int) body.Position.y,
+                         (int) line.x,
+                         (int) line.y,
+                         WHITE);
+
+            }
         }
 
         DrawFPS(10, 10);
 
         EndDrawing();
+
+        angle += 0.01f;
     }
 
     void Cleanup() {
